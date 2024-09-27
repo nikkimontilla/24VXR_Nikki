5bb3dba0262ca1b4e8487c08583a23028ab47933
nikkimontilla <francine.montilla@lethpolytech.ca>
2024-09-27 13:57:29 -0600

    Add more comments to Renderer Script

--- FL24VXR_Nikki/Assets/VXR1170/1170_Scripts/RendererComponent.cs
+++ FL24VXR_Nikki/Assets/VXR1170/1170_Scripts/RendererComponent.cs
@@ -17,28 +17,27 @@ public class RendererComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // The renderer can change the object's material and color
-
+        // The renderer can change the object's material and color - use UP and DOWN keys in this case
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            theRenderer.material.color = Color.yellow;
-            theRenderer.material.SetFloat("_Metallic", 0.9f);
+            theRenderer.material.color = Color.yellow; //Access material to change colour to yellow
+            theRenderer.material.SetFloat("_Metallic", 0.9f); //Access material to change texture into metalic
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            theRenderer.material.color = Color.gray;
-            theRenderer.material.SetFloat("_Metallic", 0f);
+            theRenderer.material.color = Color.gray; //Access material to change colour back to default gray
+            theRenderer.material.SetFloat("_Metallic", 0f); //Access material to change texture into default 
         }
-        // The renderer makes the object invisible
+        // The renderer makes the object visible and invisible - use ESC and SPACE BAR in this case
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            theRenderer.enabled = false;
+            theRenderer.enabled = false; //Set enabled to false to make object disappear
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            theRenderer.enabled = true;
+            theRenderer.enabled = true; //Set enabled to false to make object visible
 
         }
     }
