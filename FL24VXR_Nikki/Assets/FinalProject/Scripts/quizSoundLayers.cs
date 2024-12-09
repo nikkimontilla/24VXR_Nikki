e42fe4e4daf7f3946330227f4ac574adc56a9452
nikkimontilla <francine.montilla@lethpolytech.ca>
2024-12-09 12:40:08 -0700

    Sound Layers Quiz

---
+++ FL24VXR_Nikki/Assets/FinalProject/Scripts/quizSoundLayers.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quizSoundLayers : MonoBehaviour
+{
+    // Reference to ALL quiz related canvases
+    public GameObject soundLayersCanvas;
+    public GameObject quizSoundLayersCanvas;
+    public GameObject correctAnswerCanvas;
+    public GameObject wrongAnswerCanvas;
+
+    public void Start()
+    {
+        // Ensure quizSoundLayersCanvas is hidden at the start
+        quizSoundLayersCanvas.SetActive(false);
+        correctAnswerCanvas.SetActive(false);
+        wrongAnswerCanvas.SetActive(false);
+    }
+
+    // Method to be called when the quiz button is clicked
+    public void questionCanvasAppear()
+    {
+        // Check if both canvases are assigned
+        if (quizSoundLayersCanvas != null && soundLayersCanvas != null)
+        {
+            // Hide everything except the question canvas
+            soundLayersCanvas.SetActive(false);
+            correctAnswerCanvas.SetActive(false);
+            wrongAnswerCanvas.SetActive(false);
+
+            // Show the quizSoundLayersCanvas
+            quizSoundLayersCanvas.SetActive(true);
+        }
+    }
+
+    // Method to be called when correct button is clicked
+   public void correctCanvasAppear()
+    {
+        if (quizSoundLayersCanvas != null && correctAnswerCanvas != null && wrongAnswerCanvas !=null)
+        {
+            correctAnswerCanvas.SetActive(true);
+
+            quizSoundLayersCanvas.SetActive(false);
+        }
+    }
+
+    public void wrongCanvasAppear()
+    {
+        if (quizSoundLayersCanvas != null && correctAnswerCanvas != null && wrongAnswerCanvas != null)
+        {
+            wrongAnswerCanvas.SetActive(true);
+
+            quizSoundLayersCanvas.SetActive(false);
+        }
+    }
+
+    public void coolAppear()
+    {
+        soundLayersCanvas.SetActive(true);
+
+        quizSoundLayersCanvas.SetActive(false);
+        correctAnswerCanvas.SetActive(false);
+        wrongAnswerCanvas.SetActive(false);
+    }
+
+    public void tryAgainAppear()
+    {
+        questionCanvasAppear();
+    }
+   
+}
